8fbb9764dc05ac7332cb8374b8a7414346e2cc1b
OlegAbramiuk <43490849+OlegAbramiuk@users.noreply.github.com>
2019-02-02 16:06:25 +0300

    first and last comm
    yeah!!

---
+++ src/js/main.js
@@ -0,0 +1,17 @@
+//=require jquery-3.3.1.min.js
+$(function(){
+    var flag = true;
+    $('.reg').on('click', function(e){
+        e.preventDefault();
+        
+        if(flag){
+            flag = false;
+            $('.container').show('slow');
+            $('.containerReg').hide();
+        } else {
+            flag = true;
+            $('.containerReg').show('slow');
+            $('.container').hide();
+        }
+    });
+});
